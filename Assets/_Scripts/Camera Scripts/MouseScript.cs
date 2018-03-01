fd4b561cc049dd1ccc7ee7f8a0251042029c4c77
Mouath Ibrahim <Mouath.Ibrahim@gmail.com>
2018-03-01 12:07:33 -0600

    player move, camera follow, stone rune

---
+++ Assets/_Scripts/Camera Scripts/MouseScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+
+	public Texture2D cursorTexture2D;
+
+//	public GameObject mousePoint;
+	private CursorMode mode = CursorMode.ForceSoftware;
+
+	private Vector2 hotspot = Vector2.zero;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Cursor.SetCursor(cursorTexture2D, hotspot, mode);
+	}
+}
